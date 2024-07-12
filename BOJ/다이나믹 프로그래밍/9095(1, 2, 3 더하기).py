8b530c4b3d0a03efef23b20dc5b598617604f8f1
kosonkh7 <khukwangha13@khu.ac.kr>
2024-07-13 02:48:26 +0900

    Commit files

---
+++ BOJ/다이나믹 프로그래밍/9095(1, 2, 3 더하기).py
@@ -0,0 +1,16 @@
+# 정수 n이 주어졌을 때, n을 1, 2, 3의 합으로 나타내는 방법의 수를 구하는 프로그램을 작성하시오.
+
+dp = [0] * 12
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+
+for i in range(4, 12):
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+    print(dp[n])
+
