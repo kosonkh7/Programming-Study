13e97eac6cad7cd20652ed37a9773a1a711ca9c5
kosonkh7 <khukwangha13@khu.ac.kr>
2024-07-16 20:40:17 +0900

    Commit files

--- BOJ/그래프와 BFS/1707(이분 그래프).py
+++ BOJ/그래프와 BFS/1707(이분 그래프).py
@@ -4,16 +4,42 @@
 """
 
 from collections import deque
- 
+# 테스트케이스 횟수 
 k = int(input())
+
+def bfs(v):
+    queue = deque([v])
+    visited[v] = True # 방문처리
+    color[v] = 1 # 색깔 우선 1로 지정
+    while queue:
+        v = queue.popleft()
+        for i in graph[v]:
+            if not visited[i]:
+                visited[i] = True
+                color[i] = abs(1-color[v]) # 기준과 반대 색 지정
+                queue.append(i)
+            elif color[i] == color[v]: # 인접했고 이미 방문했는데, 색이 같으면 NO 반환
+                return 'NO'
+    return 'YES'
 
 
 
 for _ in range(k):
     n, m = map(int, input().split())
-    graph = [[] for x in range(n)]
-    visited = [0] * (n+1)
+    graph = [[] for x in range(n+1)]
+    visited = [False] * (n+1)
+    color = [-1] * (n+1)
+
     for i in range(m):
         a, b = map(int, input().split())
         graph[a].append(b)
-        graph[b].append(a)
+        graph[b].append(a) 
+        
+    for i in range(1, len(visited)):
+        if not visited[i]: # 방문하지 않은 노드 대상으로 bfs 실행
+            answer = bfs(i)
+            if answer =='NO': 
+                break
+        else:
+            continue
+    print(answer)
