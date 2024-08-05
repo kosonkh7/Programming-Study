ffcb449e0ed04cc52bcfe6d8baea57d0aa71b9a2
kosonkh7 <khukwangha13@khu.ac.kr>
2024-08-05 20:30:03 +0900

    Commit files

--- BOJ/브루트 포스/14501(퇴사,어려움).py
+++ BOJ/브루트 포스/14501(퇴사,어려움).py
@@ -9,7 +9,7 @@ for i in range(n):
     for j in range(i+schedule[i][0], n+1):
         if dp[j] < dp[i] + schedule[i][1]:
             dp[j] = dp[i] + schedule[i][1]
-        print(dp)
+        #print(dp)
 
 print(dp[n])
 
