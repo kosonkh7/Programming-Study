837c8712bc12d0951274eaa6de3e7d3c71960f6a
kosonkh7 <khukwangha13@khu.ac.kr>
2024-08-25 14:30:19 +0900

    Commit files

---
+++ BOJ/다이나믹 프로그래밍/11722(가장 긴 감소하는 부분 수열).py
@@ -0,0 +1,27 @@
+"""
+
+"""
+
+from collections import deque
+
+n = int(input())
+tmp = list(map(int, input().split()))
+n_list = []
+for i in range(n-1, -1, -1):
+    n_list.append(tmp[i])
+
+# 가장 긴 증가하는 부분 수열 문제로 변환
+dp = [0] * n
+dp[0] = 1
+
+for i in range(1, n):
+    x = 0
+    for j in range(i):
+        if n_list[j] < n_list[i]:
+            x = max(x, dp[j])
+    dp[i] = x + 1
+
+# print(n_list)    
+# print(dp)
+
+print(max(dp))
