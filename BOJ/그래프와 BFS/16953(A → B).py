706e2b4a02ccb774b2581834e6d8e302c31760a3
kosonkh7 <khukwangha13@khu.ac.kr>
2025-03-23 23:35:16 +0900

    Commit files

---
+++ BOJ/그래프와 BFS/16953(A → B).py
@@ -0,0 +1,24 @@
+from collections import deque
+
+a, b = map(int, input().split())
+answer = -1
+
+queue = deque([(a, 0)])
+
+while queue:
+    x, num = queue.popleft()
+    
+    if x == b:
+        answer = num
+        break
+
+    if x * 2 <= b:
+        queue.append((x*2, num+1))
+
+    if x * 10 + 1 <= b:
+        queue.append((x * 10 + 1, num+1))
+
+
+print(answer)
+    
+    
