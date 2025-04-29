ad555c44df01bae84b6f115f525be9ecaa9411cc
kosonkh7 <lovejhj09@gmail.com>
2025-04-29 17:50:24 +0900

    Commit files

--- BOJ/최단 경로/11779(최소비용 구하기 2).py
+++ BOJ/최단 경로/11779(최소비용 구하기 2).py
@@ -1,50 +1,41 @@
-"""
-다익스트라 문제에서
-1. 최소 거리 비용
-2. 최소 거리 경로 출력
-"""
-
-
-
-
-"""
-최소 거리 경로 출력, 경로 개수
-
-
 import heapq
 
 n = int(input())
 m = int(input())
 
-graph = [[] for _ in range(n+1)]  
-distance = [9999999999]*(n+1) # 최단거리 업데이트할 거리 테이블
+graph = [[] for i in range(n+1)]
+distance = [9999999999] * (n+1)
+path = [[i] for i in range(n+1)]
 
-for _ in range(m):
+for i in range(m):
     x, y, z = map(int, input().split())
     graph[x].append((y, z))
 
 start, end = map(int, input().split())
 
-def djstra(start):
+
+def dijkstra(start):
     queue = []
-    heapq.heappush(queue, (0, start)) # 최단거리 순으로 빼기 위해 거리를 앞으로
+    heapq.heappush(queue, (0, start))
     distance[start] = 0
 
     while queue:
-        d, now = heapq.heappop(queue) # 최단거리가 가장 짧은 노드 꺼내기
-        if distance[now] < d: # 현재 노드가 이미 처리된 적 있다면 무시
-            if now == end:
-                break
+        d, now = heapq.heappop(queue)
 
-        
-        for i in graph[now]: # 현재 노드와 인접한 다른 노드 확인
-            cost = d + i[1]
+        if distance[now] < d: # 이미 조회한 적 있고, 그게 더 비용이 작으면 넘기기
+            continue
 
-            if cost < distance[i[0]]:
+        for i in graph[now]:
+            cost = d + i[1] # 현재에서 다음 노드까지 가는 비용
+            if cost < distance[i[0]]: # 만약에 최소 비용 갱신 필요하면
                 distance[i[0]] = cost
                 heapq.heappush(queue, (cost, i[0]))
+                path[i[0]] = path[now] + [i[0]] # 현재까지 최소 경로에 다음 경로 이어 붙임
 
+dijkstra(start)
 
-djstra(start)
+print(distance[end])
+print(len(path[end]))
 
-print(distance[end])"""
+for i in path[end]:
+    print(i, end=' ')
